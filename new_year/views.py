12aa32f037e0710d5208a46a412dfab06067985d
Eliza Ahmed <elizarafee@gmail.com>
2020-12-02 15:55:24 +0000

    new_year app updated

--- new_year/views.py
+++ new_year/views.py
@@ -8,5 +8,6 @@ def newyear(request):
     date = datetime.datetime.now()
 
     return render(request, "new_year/newyear.html", {
-        "ans" : date.month == 1 and date.day == 1 
+        "ans" : date.month == 1 and date.day == 1,
+        "date" : date
     })

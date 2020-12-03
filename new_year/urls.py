fc3a0a0ed5340d851e3147a4ea0983bca4062905
Eliza Ahmed <elizarafee@gmail.com>
2020-12-03 08:50:09 +0000

    linking directly to any routeinstead writing the full url

--- new_year/urls.py
+++ new_year/urls.py
@@ -2,6 +2,7 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
+    # this url
     path('', views.newyear, name="newyear"),
     path('extending', views.extending, name="extending")
 ]

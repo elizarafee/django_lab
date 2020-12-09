d803d73ccec3be8ff8c2e570c89d4e140502f43b
Eliza Ahmed <elizarafee@gmail.com>
2020-12-09 14:04:07 +0000

    server-side validation

--- taskform/views.py
+++ taskform/views.py
@@ -1,15 +1,30 @@
 from django.shortcuts import render
 from django import forms
 
+tasks = []
+
 class NewTaskForm(forms.Form):
     taskname = forms.CharField(label="Task Name")
-    priority = forms.IntegerField(label="Priority", max_value=10, min_value=2)
+    # priority = forms.IntegerField(label="Priority", max_value=10, min_value=2)
 
 # Create your views here.
 def mainform(request):
+    if request.method=="POST":
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            task = form.cleaned_data("taskname")
+            tasks.append(task)
+            return render(request, "taskform/tasks.html")
+        else:
+            return render(request, "taskform/tasks.html", {
+                "form" : form
+            })
+
     return render(request, "taskform/dataform.html", {
-        "task" : NewTaskForm()
+        "form" : NewTaskForm()
     })
 
-def tasks(request):
-    return render(request, "taskform/tasks.html")
+def tasks(request, tasks):
+    return render(request, "taskform/tasks.html", {
+        "tasks" : tasks
+    })
